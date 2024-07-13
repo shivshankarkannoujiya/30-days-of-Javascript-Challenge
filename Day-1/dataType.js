205677c032a78230220bc4c43faab96ee1ed7888
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-07-13 12:15:56 +0530

    Day-1 Task done

---
+++ Day-1/dataType.js
@@ -0,0 +1,31 @@
+// TODO: Create variables of Different Datatypes (number, string, boolean, object, array ) and log each variables type using the "typeof() Operator"
+
+const score =  100;
+console.log(`Type of NUMBER : ${typeof score}`);
+
+
+const userName = "abhi";
+console.log(`Type of  STRING : ${typeof userName}`);
+
+
+const isLoggedIn = true;
+console.log(`Type of  BOOLEAN : ${typeof isLoggedIn}`);
+
+
+const myObject = {
+    name: "Abhishek",
+    age : 22,
+    email : "abhi@xyz",
+};
+console.log(`Type of  OBJECT : ${typeof myObject}`);
+
+
+
+const myArray  = [2,3,4,5,6,7,"rock",true,5.6,{name:"ABhishek",age:22}];
+console.log(`Type of ARRAY : ${typeof myArray}`);
+
+
+/*
+ * Object : Object
+ * Array : Object
+ */
---
+++ Day-1/variables.js
@@ -0,0 +1,28 @@
+// TODO: Devlare a variable using "var", assign it a number, and log value to the console.
+// var score  = 90;
+// console.log(`Score is : ${score}`); 
+
+
+// TODO: Devlare a variable using "let", assign it a string, and log value to the console.
+// let userName = "Abhishek";
+// console.log(`Username : ${userName}`);
+
+// TODO: Devlare a variable using "const", assign it a boolean value, and log value to the console.
+// const isLoggedIn = true;
+// console.log(`User Login : ${isLoggedIn}`);
+
+
+// TODO: Declare a Variable using "let" assing it an initial value, reassign a new value, and log both value to the console
+// let physcicsMarks = 90;
+// console.log(`Phyics Marks is : ${physcicsMarks}`);
+// physcicsMarks = 100;
+// console.log(`Phyics Marks is : ${physcicsMarks}`);
+
+
+// TODO: Understanding const 
+// TODO: Try reassigning a variable declared with const and observe the Error
+
+// const firstName = "Rock";
+// firstName = "Jack"; //! TypeError: Assignment to constant variable.
+// console.log(`First name is : ${firstName}`);
+
