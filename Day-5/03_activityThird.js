efbf7a1c641a75898d5fc2130d4f3c9d07bd497f
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-07-18 18:41:08 +0530

    chore: Add code for Day 5 activities

---
+++ Day-5/03_activityThird.js
@@ -0,0 +1,23 @@
+
+//! Task - 5
+const sum = (num1, num2) => num1 + num2;
+// console.log(sum(5,4));
+
+
+//! Task - 6
+// indexof()
+// includes
+
+// const isContainChar = (str,char) =>{
+//     if (str.includes(char)) {
+//         return true
+//     }
+//     return false;
+// }
+
+
+const isContainChar = (str,char) => str.includes(char);
+
+const name = "Abhishek";
+char = 'A'
+console.log(isContainChar(name,char));
---
+++ Day-5/04_activityFourth.js
@@ -0,0 +1,8 @@
+//! Task - 7
+// const getProduct = (num1, num2 = 12) => num1*num2;
+// console.log(getProduct(12));
+
+
+//! Task - 8
+// const greetMessage = (name,age = 18) => console.log(`${name}, welcome to website your age is : ${age} `);
+// console.log(greetMessage('Rock'));
