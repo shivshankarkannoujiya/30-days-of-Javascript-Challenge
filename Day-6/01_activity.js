73c89f726be435a7d178b4c7a7c3b2a4e7736354
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-07-19 18:16:01 +0530

    Add code for Day 6 activity 2

---
+++ Day-6/01_activity.js
@@ -0,0 +1,21 @@
+// TODO: Activity - 1 
+// const myArr = [1,2,3,4,5];
+// console.log(myArr[0]);
+// console.log(myArr[myArr.length - 1])
+
+// TODO: Activity - 2 :  Array Methods (Basics)
+const myArr = [1,2,3,4,5];
+// myArr.push(6)
+// console.log(myArr);
+
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr);
+
+myArr.unshift(10);
+console.log(myArr);
+
+
+
