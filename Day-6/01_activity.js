708f01b3e063bd9461eb3f1bbf89ef52b88f76e2
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-07-24 22:12:46 +0530

    chore: Refactor Day 6 activity 2 code for better readability and remove commented out code

--- Day-6/01_activity.js
+++ Day-6/01_activity.js
@@ -4,7 +4,7 @@
 // console.log(myArr[myArr.length - 1])
 
 // TODO: Activity - 2 :  Array Methods (Basics)
-const myArr = [1,2,3,4,5];
+// const myArr = [1,2,3,4,5];
 // myArr.push(6)
 // console.log(myArr);
 
@@ -14,8 +14,26 @@ const myArr = [1,2,3,4,5];
 // myArr.shift()
 // console.log(myArr);
 
-myArr.unshift(10);
-console.log(myArr);
+// myArr.unshift(10);
+// console.log(myArr);
+
+
+// TODO: Activity - 3 :  Array Methods (Intermediate)
+const myArr = [1,2,3,4,5];
+
+//! map 
+// const newArr = myArr.map((num) => num*2);
+// console.log(newArr);
+
+//! filter
+// const evenArr = myArr.filter((num)=>(num%2==0))
+// console.log(evenArr);
+
+//! reduce
+// const totalSum = myArr.reduce( (accumulator,curentValue) => (accumulator + curentValue),0);
+// console.log(totalSum);
+
+
 
 
 
