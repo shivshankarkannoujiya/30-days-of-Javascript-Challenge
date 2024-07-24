5cb69a4a82324f38cdee8d0807e52e8647daef7f
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-07-24 22:03:22 +0530

    feat: Add code for displaying book details in the library

---
+++ Day-7/02_objects.js
@@ -0,0 +1,35 @@
+const library = {
+    name : "International Library",
+
+    books : [
+        {
+            title : "The Art of Invisibility",
+            author : "Kevin Mitnick",
+            yearPublished : 2017
+        },
+
+        {
+            title: "Cybersecurity and Cyberwar: What Everyone Needs to Know",
+            Author: "P.W. Singer and Allan Friedman",
+            yearPublished: 2014,
+        },
+
+        {
+            title: "Hacking: The Art of Exploitation",
+            Author: "Jon Erickson",
+            yearPublished: 2008,
+        }
+    ]
+}
+// console.log(library);
+// console.log(library.name);
+
+// const booksTitle = library.books.map(books => books.title);
+// console.log(booksTitle);
+
+
+for (const key in library.books) {
+    console.log(`Title : ${library.books[key].title}`);
+    console.log(`Author : ${library.books[key].Author}`);
+    console.log(`Published : ${library.books[key].yearPublished}`);
+}
